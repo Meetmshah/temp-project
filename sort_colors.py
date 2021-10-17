4085642762415fb12e1f19c517101529512be1f0
MeetShah-Yo <73047974+MeetShah-Yo@users.noreply.github.com>
2021-10-17 10:31:55 +0530

    Update sort_colors.py
    # We can update input and output method.

--- sort_colors.py
+++ sort_colors.py
@@ -26,3 +26,5 @@ if __name__ == "__main__":
     nums = [0, 1, 1, 1, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2]
     sort_colors(nums)
     print(nums)
+
+# We can update input and output method.
